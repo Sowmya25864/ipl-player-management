e17aa5abb0f77b9ca9fe11cb2e166b322dcaa4b2
Sowmya <sowmyatadikamalla143@gmail.com>
2025-08-08 22:01:00 +0530

    Added PlayerView class

---
+++ View.py
@@ -0,0 +1,13 @@
+class PlayerView:
+    @staticmethod
+    def display_player(player):
+        print(player)
+
+    @staticmethod
+    def display_all_players(players):
+        for player in players:
+            print(player)
+
+    @staticmethod
+    def display_message(message):
+        print(message)
