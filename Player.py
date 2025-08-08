21fcaa7fe8e7112629c3c44c7ea9b052c24a3afc
Sowmya <sowmyatadikamalla143@gmail.com>
2025-08-08 21:59:39 +0530

    Added Player model class

--- Player.py
+++ Player.py
@@ -1,5 +1,3 @@
-# Player.py
-
 class Player:
     def __init__(self, player_id, name, team, role, runs=0, wickets=0):
         self.player_id = player_id
@@ -8,3 +6,12 @@ class Player:
         self.role = role
         self.runs = runs
         self.wickets = wickets
+
+    def update_runs(self, runs):
+        self.runs += runs
+
+    def update_wickets(self, wickets):
+        self.wickets += wickets
+
+    def __str__(self):
+        return f"ID: {self.player_id}, Name: {self.name}, Team: {self.team}, Role: {self.role}, Runs: {self.runs}, Wickets: {self.wickets}"
